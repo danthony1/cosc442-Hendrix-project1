1eddee5d6ea1d8d18557b95b5a92503bb91fcb02
danthony1 <tracker346@comcast.net>
2016-02-19 22:53:58 -0500

    Finished product

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,5 +1,18 @@
 
 public class HelloWorld {
+	
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 * -
+	 * I have never used eclipse before so 
+	 * I learned the basics of how to deal 
+	 * with the compiling and running of 
+	 * eclipse as well as making and handling 
+	 * projects. I have also never used the 
+	 * marketplace on an IDE.
+	 */
 	public static void main(String args[]){
 		System.out.println("Hello World");
 	}
